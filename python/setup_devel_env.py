a0634d11cc2d3fcfb50ead4221c51de59d9af881
Sogartar <sogartary@yahoo.com>
2020-11-23 14:31:18 -0800

    Add pre-commit to devel env setup

--- python/setup_devel_env.py
+++ python/setup_devel_env.py
@@ -1,5 +1,12 @@
 #!/usr/bin/env python3
 
 from subprocess import run
+from os import path
 
-run(["pip", "install", "numpy>=1.10", "scipy>=1.4", "wheel", "twine"], check=True)
+run(
+    ["pip", "install", "numpy>=1.10", "scipy>=1.4", "wheel", "twine", "pre-commit"],
+    check=True,
+)
+
+curr_script_dir = path.abspath(path.dirname(__file__))
+run(["pre-commit", "install"], check=True, cwd=path.join(curr_script_dir, ".."))
